bbe4ee5a6852ec7c841df5c9e3cc37e9a8d829b7
tatiana <bosenko.tanya@gmail.com>
2026-03-21 22:03:19 +0200

    Add jsBasics homework

---
+++ jsBasics/task1.js
@@ -0,0 +1,12 @@
+let age = 38
+let firstName = `Tanya`
+let hasChildren = false
+let nullVar = null
+let undVar = undefined
+
+console.log(age)
+console.log(firstName)
+console.log(hasChildren)
+console.log(nullVar)
+console.log(undVar)
+
